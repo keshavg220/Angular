0c3ea3a4e07e6601bfdfb939d7d1a3095aca4ca8
keshav220 <59433700+keshavg220@users.noreply.github.com>
2020-06-05 03:14:17 +0530

    update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -20,6 +25,9 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     MatListModule
   ],
   providers: [],
