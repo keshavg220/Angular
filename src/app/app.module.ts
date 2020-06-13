c3003323f4039b737bd4176acb3f4c82279ed29b
keshav220 <59433700+keshavg220@users.noreply.github.com>
2020-06-14 02:03:22 +0530

    Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
 
@@ -60,8 +64,11 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatButtonModule,
     MatListModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    ReactiveFormsModule
 
   ],
   providers: [
