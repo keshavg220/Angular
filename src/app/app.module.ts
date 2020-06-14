912a210564c72cd518a3b41ce18d09e6df21a366
keshav220 <59433700+keshavg220@users.noreply.github.com>
2020-06-14 14:59:06 +0530

    Promises while delay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -68,6 +69,7 @@ import { LoginComponent } from './login/login.component';
     MatSlideToggleModule,
     AppRoutingModule,
     MatDialogModule,
+    MatProgressSpinnerModule,
     ReactiveFormsModule
 
   ],
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { Leaders } from '../shared/leaders';
+import { resolve } from 'url';
 
 
 @Injectable({
@@ -8,15 +9,23 @@ import { Leaders } from '../shared/leaders';
 })
 export class LeaderService {
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(Leaders);
+    return new Promise(
+      resolve => {
+        setTimeout(() => resolve(Leaders),2000);
+      });
   }
   
   getLeader(id: string): Promise<Leader> {
-    return Promise.resolve(Leaders.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(Leaders.filter((leader) => (leader.id === id))[0]),2000);
+    });
+      
   }
   
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(Leaders.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve => {
+    setTimeout(() => resolve(Leaders.filter((leader) => leader.featured)[0]),2000);
+    });
   }
   
 
